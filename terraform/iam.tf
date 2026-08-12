0b62ec6b149b57260276c264b9a89a0f45cc3f6a
Prince Hethvadiya <prince.h@ahduni.edu.in>
2026-08-12 16:18:30 +0530

    fix(iam): strictly scope OIDC trust policy to production environment subject

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -164,12 +164,8 @@ resource "aws_iam_role" "github_actions_deploy" {
         Federated = aws_iam_openid_connect_provider.github_actions.arn
       }
       Condition = {
-        StringLike = {
-          # Actual sub claim from GitHub OIDC includes internal numeric IDs:
-          # repo:prinsumaster@244661261/PariLink@1328853957:<ref or environment>
-          "token.actions.githubusercontent.com:sub" = "repo:prinsumaster*PariLink*:*"
-        }
         StringEquals = {
+          "token.actions.githubusercontent.com:sub" = "repo:prinsumaster@244661261/PariLink@1328853957:environment:production"
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
       }
