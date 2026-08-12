b11a39dc2e6275e6da454caa9daea75741ef020f
Prince Hethvadiya <prince.h@ahduni.edu.in>
2026-08-12 16:45:19 +0530

    fix: update OIDC provider thumbprints for modern GitHub Actions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -149,7 +149,12 @@ resource "aws_iam_openid_connect_provider" "github_actions" {
 
   client_id_list = ["sts.amazonaws.com"]
 
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  # AWS now recommends providing these active thumbprints for GitHub Actions OIDC:
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+    "1b511abead59c6ce207077c0bf0e0043b1382612"
+  ]
 }
 
 resource "aws_iam_role" "github_actions_deploy" {
